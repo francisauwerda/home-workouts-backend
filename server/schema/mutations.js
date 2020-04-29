49efc72d857d6af5368828fbd827e520a0b6f9be
Francis Auwerda <fauwerda@gmail.com>
2020-04-29 08:20:49 +0200

    Add delete workout mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -2,6 +2,7 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
 const mongoose = require('mongoose');
 const Workout = mongoose.model('workout');
+const Exercise = mongoose.model('exercise');
 const WorkoutType = require('./workout_type');
 
 const mutation = new GraphQLObjectType({
@@ -24,6 +25,18 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parentValue, { title, workoutId }) {
         return Workout.addExercise(workoutId, title);
+      }
+    },
+    deleteWorkout: {
+      type: WorkoutType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve(parentValue, { id }) {
+        return Promise.all([
+          Exercise.deleteMany({ workout: id }),
+          Workout.deleteOne({ _id: id })
+        ])
       }
     }
   }
