67e97978c429e4a3f834b225702ef3e35b752323
Francis Auwerda <fauwerda@gmail.com>
2020-06-04 08:26:15 +0200

    Bulk import Workout A

--- server/scripts/bulkAddWorkoutAndExercises.js
+++ server/scripts/bulkAddWorkoutAndExercises.js
@@ -4,22 +4,87 @@ const createWorkout = require('./createWorkout');
 const createExercise = require('./createExercise');
 
 const fakeData = {
-  workoutName: 'Nelis workout',
+  workoutName: 'Workout A',
   exercises: [{
-    exerciseName: 'Dips2',
-    // imagePath: 'images/dips.jpg',
-    // order: 1
+    exerciseName: 'Alt. Single Leg Box Squats',
+    imagePath: 'images/0.png',
+    order: 1
   }
-  // ,{
-  //   exerciseName: 'Jumping squats',
-  //   imagePath: 'images/dips.jpg',
-  //   order: 3
-  // }
-  // ,{
-  //   exerciseName: 'No image',
-  //   // imagePath: 'images/dips.jpg',
-  //   // order: 3
-  // }
+  ,{
+    exerciseName: '1 1/2 Bottomed Out Squats',
+    imagePath: 'images/1.png',
+    order: 2
+  }
+  ,{
+    exerciseName: 'Jump Squats',
+    imagePath: 'images/2.png',
+    order: 3
+  }
+  ,{
+    exerciseName: 'Handstand Pushups/Power Pushaway',
+    imagePath: 'images/3.png',
+    order: 4
+  }
+  ,{
+    exerciseName: 'Rotational Pushups',
+    imagePath: 'images/4.png',
+    order: 5
+  }
+  ,{
+    exerciseName: 'Cobra Pushups',
+    imagePath: 'images/5.png',
+    order: 6
+  }
+  ,{
+    exerciseName: 'Alt. Single Leg Heel Touch [Kickstand] Squats',
+    imagePath: 'images/6.png',
+    order: 7
+  }
+  ,{
+    exerciseName: 'Alt. Sprinter Lunges',
+    imagePath: 'images/7.png',
+    order: 8
+  }
+  ,{
+    exerciseName: 'Plyo Sprinter Lunges',
+    imagePath: 'images/8.png',
+    order: 9
+  }
+  ,{
+    exerciseName: 'Pullups',
+    imagePath: 'images/9.png',
+    order: 10
+  }
+  ,{
+    exerciseName: 'Human Pullovers/Sliding Pulldowns',
+    imagePath: 'images/10.png',
+    order: 11
+  }
+  ,{
+    exerciseName: 'Inverted Chin Curls',
+    imagePath: 'images/11.png',
+    order: 12
+  }
+  ,{
+    exerciseName: 'Reverse Corkscrews',
+    imagePath: 'images/12.png',
+    order: 13
+  }
+  ,{
+    exerciseName: 'Black Widow Knee Slides',
+    imagePath: 'images/13.png',
+    order: 14
+  }
+  ,{
+    exerciseName: 'Levitation Crunches',
+    imagePath: 'images/14.png',
+    order: 15
+  }
+  ,{
+    exerciseName: 'Angels and Devils',
+    imagePath: 'images/15.png',
+    order: 16
+  }
 ]
 }
 
